9a74f06801e36d67ad729f8ce6ae7153f556f52e
JGanning <joseph.ganning@gmail.com>
2021-04-13 15:42:18 -0400

    Updates

---
+++ precache-manifest.3491380b04ef3b852607f9bb88c67ca6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480ba60eb0370f9e51a3679f69eed6dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907f4a24ac5c084f590e",
+    "url": "/static/css/2.97ea2a57.chunk.css"
+  },
+  {
+    "revision": "3f1019102d3546e57982",
+    "url": "/static/css/main.fc7e7d4d.chunk.css"
+  },
+  {
+    "revision": "907f4a24ac5c084f590e",
+    "url": "/static/js/2.038c0254.chunk.js"
+  },
+  {
+    "revision": "f596a8d35c481d1ce17d2d96360a963b",
+    "url": "/static/js/2.038c0254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1019102d3546e57982",
+    "url": "/static/js/main.e3630a75.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "21f4f5d2abde26eb6de46d3d3291e7e3",
+    "url": "/static/media/Bensound - Hey.21f4f5d2.mp3"
+  },
+  {
+    "revision": "15e0ab24d7ba0cf97151de005bc5225b",
+    "url": "/static/media/Bensound - Sunny.15e0ab24.mp3"
+  },
+  {
+    "revision": "c3fe3f45ebdbfe3e88f1df3badb151f9",
+    "url": "/static/media/Castle of Glass - Linkin Park.c3fe3f45.mp3"
+  },
+  {
+    "revision": "f8f0ff605206a1ed10c29930c6b5d2ae",
+    "url": "/static/media/DKWallpaper.f8f0ff60.jpg"
+  },
+  {
+    "revision": "5d30b1beba9045fe23d86af58979c179",
+    "url": "/static/media/Famous for Nothing - Dropkick Murphys.5d30b1be.mp3"
+  },
+  {
+    "revision": "27dd19344c24697277d24ce1e8210102",
+    "url": "/static/media/Guzma - Pokemon.27dd1934.mp3"
+  },
+  {
+    "revision": "281a4fb2ea6d482b6a77f01e763952a3",
+    "url": "/static/media/Guzma - Pokemon.281a4fb2.PNG"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/Hamburger_icon.86595150.svg"
+  },
+  {
+    "revision": "e113921618959deb1859052dfe3d3400",
+    "url": "/static/media/Logo.e1139216.png"
+  },
+  {
+    "revision": "66f75e5f2d31c7be641deb56e0807021",
+    "url": "/static/media/Pallet - Pokemon.66f75e5f.PNG"
+  },
+  {
+    "revision": "ea1ca771fb99206695d296103dec4ecb",
+    "url": "/static/media/Pallet - Pokemon.ea1ca771.mp3"
+  },
+  {
+    "revision": "8da7ae637760cce30a328c8fdbded9a1",
+    "url": "/static/media/Zinnia - Pokemon.8da7ae63.mp3"
+  },
+  {
+    "revision": "facbbfd1798e26f9ed6d1401718c8736",
+    "url": "/static/media/Zinnia - Pokemon.facbbfd1.PNG"
+  },
+  {
+    "revision": "17f56e8caa8a52f71cbd1d7a04614790",
+    "url": "/static/media/josephganning_resume.17f56e8c.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc2853f657d8dc74454a8fe3f1d0f5ef",
+    "url": "/static/media/shrek.dc2853f6.PNG"
+  }
+]);
